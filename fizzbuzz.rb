75c9de36840a69ad0e9b32a6437c9dce4b5b5b3b
Nathan Rymarz <nrymarz@gmail.com>
2020-09-24 23:57:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 ==0 
-    "Fizz"
+  s=nil
+  if int % 3 == 0 
+    s="Fizz"
+  elsif int % 5 == 0
+    s=s+"Buzz"
+  s
+end
