8db1bab81306743e35732fa16a6388d818fb5c67
Nathan Rymarz <nrymarz@gmail.com>
2020-09-25 00:20:33 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  s=nil
+  if int % 3 == 0 
+    s="Fizz"
+  end
+  if int % 5 == 0
+    s=s.to_s + "Buzz"
+  end
+  s
+end
