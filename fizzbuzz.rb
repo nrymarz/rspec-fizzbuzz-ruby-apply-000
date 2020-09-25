5fc92347ca6836999044fe45d3d1844b544d1429
Nathan Rymarz <nrymarz@gmail.com>
2020-09-25 00:09:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  s=''
   if int % 3 == 0 
     s="Fizz"
   elsif int % 5 == 0
